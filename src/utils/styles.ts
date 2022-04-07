5a521d557dc2ac9b313a94320867f492c048a948
Luciano Aedo Puga <shvme.development@gmail.com>
2022-04-07 14:57:39 -0400

    feat: add a utility with styles for console output

---
+++ src/utils/styles.ts
@@ -0,0 +1,52 @@
+
+// STYLES
+export const RESET = '\u001b[0m';
+export const BOLD = '\u001b[1m';
+export const DIM = '\u001b[2m';
+export const ITALIC = '\u001b[3m';
+export const UNDERLINE = '\u001b[4m';
+export const INVERSE = '\u001b[7m';
+export const HIDDEN = '\u001b[8m';
+export const STRIKETHROUGH = '\u001b[9m';
+
+// COLORS
+export const BLACK = '\u001b[30m';
+export const RED = '\u001b[31m';
+export const GREEN = '\u001b[32m';
+export const YELLOW = '\u001b[33m';
+export const BLUE = '\u001b[34m';
+export const MAGENTA = '\u001b[35m';
+export const CYAN = '\u001b[36m';
+export const WHITE = '\u001b[37m';
+export const GRAY = '\u001b[90m';
+export const GREY = '\u001b[90m';
+
+// BRIGHT COLORS
+export const BRIGHT_RED = '\u001b[91m';
+export const BRIGHT_GREEN = '\u001b[92m';
+export const BRIGHT_YELLOW = '\u001b[93m';
+export const BRIGHT_BLUE = '\u001b[94m';
+export const BRIGHT_MAGENTA = '\u001b[95m';
+export const BRIGHT_CYAN = '\u001b[96m';
+export const BRIGHT_WHITE = '\u001b[97m';
+
+// BACKGROUNDS
+export const BLACK_BACKGROUND = '\u001b[40m';
+export const RED_BACKGROUND = '\u001b[41m';
+export const GREEN_BACKGROUND = '\u001b[42m';
+export const YELLOW_BACKGROUND = '\u001b[43m';
+export const BLUE_BACKGROUND = '\u001b[44m';
+export const MAGENTA_BACKGROUND = '\u001b[45m';
+export const CYAN_BACKGROUND = '\u001b[46m';
+export const WHITE_BACKGROUND = '\u001b[47m';
+export const GRAY_BACKGROUND = '\u001b[100m';
+export const GREY_BACKGROUND = '\u001b[100m';
+
+// BRIGHT BACKGROUNDS
+export const BRIGHT_RED_BACKGROUND = '\u001b[101m';
+export const BRIGHT_GREEN_BACKGROUND = '\u001b[102m';
+export const BRIGHT_YELLOW_BACKGROUND = '\u001b[103m';
+export const BRIGHT_BLUE_BACKGROUND = '\u001b[104m';
+export const BRIGHT_MAGENTA_BACKGROUND = '\u001b[105m';
+export const BRIGHT_CYAN_BACKGROUND = '\u001b[106m';
+export const BRIGHT_WHITE_BACKGROUND = '\u001b[107m';
