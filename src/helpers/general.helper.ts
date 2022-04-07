969e4cd921820b97e82c755ed8e8c19b56f4fd11
Luciano Aedo Puga <shvme.development@gmail.com>
2022-04-07 14:56:31 -0400

    feat: add a helper to convert variables to text

--- src/helpers/general.helper.ts
+++ src/helpers/general.helper.ts
@@ -2,6 +2,25 @@
 import callerPath from 'caller-path';
 
 // FUNCTION
+export function stringify ( value: unknown ): string {
+
+	let output = value;
+
+	if ( typeof value === 'string' ) output = `'${ value }'`;
+	else if ( typeof value === 'undefined' ) output = 'undefined';
+	else if ( value === null ) output = 'null';
+	else if ( Number.isNaN( value ) ) output = 'NaN';
+	else if ( typeof value === 'boolean' ) output = value.toString();
+	else if ( typeof value === 'number' ) output = value.toString();
+	else if ( typeof value === 'symbol' ) output = value.toString();
+	else if ( typeof value === 'function' ) output = value.name;
+	else if ( typeof value === 'object' ) output = JSON.stringify( value );
+
+	return `${ output }`;
+
+}
+
+// FUNCTION
 export function caller ( depth: number = 0 ): string {
 
 	const path = callerPath( { depth: depth } );
